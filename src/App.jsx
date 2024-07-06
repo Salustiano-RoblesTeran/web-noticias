ee6ac3bcadf5c801b7157690d01ac1ecbd1f29ca
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-06 13:34:19 -0300

    termine el proyecto de noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Formulario from "./components/Formulario";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
     <>
-    
+    <Container className="mainSection">
+      <h1 className="text-center mt-5 text-light">Noticias</h1>
+      <hr  className="text-light" />
+      <Formulario />
+    </Container>
+      <footer className="bg-dark text-light text-center py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
 export default App
